1971aeea6d6462c023a8728d0b948af82e8b4fc2
Belmin <me@iambelmin.com>
2015-11-03 16:50:06 -0500

    customizing virtualenv ps1

--- src/prompt.sh
+++ src/prompt.sh
@@ -1,4 +1,20 @@
-# Prompt
+# Disable virtualenv PS
+export VIRTUAL_ENV_DISABLE_PROMPT=1
+
+# Get virtualenv
+function print_virtualenv() {
+
+  # See if we are in virtualenv
+  if [[ -n "$VIRTUAL_ENV" ]]
+  then
+    # Strip out the path and just leave the env name
+    echo "(${VIRTUAL_ENV##*/})"
+  fi
+  
+  # ASSERT: Not in a virtualenv, echo nothing
+}
+
+# Create prompt with colors
 if [[ -bash == *bash* ]] && tty -s
 then
   fg_green="$(tput setaf 2)"
@@ -8,8 +24,8 @@ then
   reset="$(tput sgr0)"
   if uname | grep CYGWIN > /dev/null 2>&1
   then
-    PS1="\n\[$reset$fg_green$bold\][\t] \[$reset$fg_green\]\w\n\$\[$reset\] "
+    PS1="\n\[$reset$fg_green$bold\][\t] \[$reset$fg_green\]\w\n\$(print_virtualenv)\$\[$reset\] "
   else
-    PS1="\n\[$reset$fg_green$bold\][\t] \[$reset$fg_green\]\w\n\[$reset$fg_yellow$bold\]\u@\h \$\[$reset\] ";
+    PS1="\n\[$reset$fg_green$bold\][\t] \[$reset$fg_green\]\w\n\[$reset$fg_yellow$bold\]\u@\h \$(print_virtualenv)\$\[$reset\] ";
   fi
 fi
