52604b24493f2ae738f96394a14c12ed393af49e
Belmin <me@iambelmin.com>
2015-12-08 09:27:42 -0500

    modified history

--- src/history.sh
+++ src/history.sh
@@ -1,7 +1,17 @@
 # History settings
 export HISTSIZE=1000000
-export HISTCONTROL='ignoreboth'
+
+# Ignore commands that start with spaces
+export HISTCONTROL='ignorespace'
+
+# Ignore ls,bg,fg,exit
 export HISTIGNORE='&:ls:[bf]g:exit'
+
+# Add time to history
 export HISTTIMEFORMAT='%b %d %H:%M:%S: '
+
+# Save history right away
+export PROMPT_COMMAND='history -a'
+
 shopt -s histappend
 shopt -s cmdhist
