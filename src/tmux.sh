c57251ffbce7a94f8854c3a31a4a5de4eca35e06
Belmin <me@iambelmin.com>
2016-01-26 17:40:41 -0500

    hide ssh agent file

--- src/tmux.sh
+++ src/tmux.sh
@@ -1,5 +1,5 @@
 # Consistent SSH auth sock to work around tmux issue
-SOCK_PATH="$HOME/ssh-agent-$USER-tmux"
+SOCK_PATH="$HOME/.ssh-agent-$USER-tmux"
 if test $SSH_AUTH_SOCK && [ $SSH_AUTH_SOCK != $SOCK_PATH ]
 then
   unlink $SOCK_PATH 2> /dev/null
