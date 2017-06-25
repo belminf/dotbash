f89b2f6f43ab845820cdc19e9d07465ccb5b96c6
Belmin <belminf@gmail.com>
2017-06-25 14:45:14 -0400

    added CFLAGS env

--- src/cygwin.sh
+++ src/cygwin.sh
@@ -2,4 +2,7 @@
 if uname | grep CYGWIN > /dev/null 2>&1
 then
   eval $(ssh-pageant -ra /.ssh-pageant)
+
+  # For Ansible, per https://github.com/belminf/ansible-playbooks
+  export CFLAGS="-D_DEFAULT_SOURCE"
 fi
