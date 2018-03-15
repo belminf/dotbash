d448cec76abd088f9b80e6791d108d4b867f5a46
Belmin Fernandez <belminf@gmail.com>
2018-03-15 16:35:35 -0400

    another attempt at ssh-agent + tmux compat

--- src/tmux.sh
+++ src/tmux.sh
@@ -1,11 +1,7 @@
 # Fixes ssh-agent issue with tmux
 
-# Run ssh-agent if not already running
-pgrep -u $USER ssh-agent > /dev/null 2>&1 || ssh-agent > /dev/null 2>&1
-
 # Set new SSH_AUTH_SOCK
 export SSH_AUTH_SOCK="${HOME}/.ssh/.auth_sock.$(hostname)"
 
-# Link SSH agent file
-CURRENT_AUTH_SOCK="$(find /tmp -type s -printf "%T@ %p\n" 2> /dev/null | grep ssh | sort -n  | cut -d' ' -f 2- | tail -n 1)"
-ln -sf "$CURRENT_AUTH_SOCK" "$SSH_AUTH_SOCK"
+# Run ssh-agent if not already running
+pgrep -u $USER ssh-agent > /dev/null 2>&1 || echo "hi" && ssh-agent -a $SSH_AUTH_SOCK > /dev/null 2>&1
