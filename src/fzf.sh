eb2655294a44fba51d151575b781aa980101cf15
Belmin Fernandez <belminf@gmail.com>
2018-03-26 23:45:08 -0400

    supporting fzf

---
+++ src/fzf.sh
@@ -0,0 +1,15 @@
+# If this is a user setup, add to PATH
+if [ -d "${HOME}/.fzf/bin" ] && [[ ! "$PATH" == *${HOME}/.fzf/bin* ]]
+then
+  FZF_SCRIPTS_DIR="${HOME}/.fzf/shell"
+  export PATH="$PATH:${HOME}/.fzf/bin"
+
+# Check if installed system wide
+elif [ -d "/usr/share/fzf" ]
+then
+  FZF_SCRIPTS_DIR="/usr/share/fzf"
+fi
+
+# Source FZF bash files
+source "${FZF_DIR}/completion.bash"
+source "${FZF_DIR}/key-bindings.bash"
