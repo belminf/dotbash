6c1228c813f5f97fc1c1b337400516e4daec934d
Belmin <belminf@gmail.com>
2018-03-28 17:11:26 -0400

    catch exception

--- src/tmux.sh
+++ src/tmux.sh
@@ -9,5 +9,5 @@ pgrep -u $USER ssh-agent > /dev/null 2>&1 || ssh-agent -a $SSH_AUTH_SOCK > /dev/
 # If auth sock is a symlink, update it
 if [ -L $SSH_AUTH_SOCK ] || ! [ -e $SSH_AUTH_SOCK ]
 then
-    ln -sf $(ls -dt1 /tmp/ssh-*/* | head -n 1) $SSH_AUTH_SOCK
+    ln -sf $(ls -dt1 /tmp/ssh-*/* | head -n 1) $SSH_AUTH_SOCK 2> /dev/null
 fi
