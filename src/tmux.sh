58a4f62ff17d263c0fe93d3c94db5b934fc61dde
Belmin <belminf@gmail.com>
2018-03-29 21:18:42 -0400

    cleaning up ssh-agent socket before running

--- src/tmux.sh
+++ src/tmux.sh
@@ -4,7 +4,7 @@
 export SSH_AUTH_SOCK="${HOME}/.ssh/.auth_sock.$(hostname)"
 
 # Run ssh-agent if not already running
-pgrep -u $USER ssh-agent > /dev/null 2>&1 || ssh-agent -a $SSH_AUTH_SOCK > /dev/null 2>&1
+pgrep -u $USER ssh-agent > /dev/null 2>&1 || rm -f $SSH_AUTH_SOCK 2> /dev/null; ssh-agent -a $SSH_AUTH_SOCK > /dev/null 2>&1
 
 # If auth sock is a symlink, update it
 if [ -L $SSH_AUTH_SOCK ] || ! [ -e $SSH_AUTH_SOCK ]
