ffea34b3000331ba62d38618d428cdd48150ed53
Belmin <belminf@gmail.com>
2018-04-04 21:07:26 -0400

    tweaking this again!

--- src/tmux.sh
+++ src/tmux.sh
@@ -1,10 +1,9 @@
 # Fixes ssh-agent issue with tmux
+export SSH_AUTH_SOCK="${SSH_AUTH_SOCK:-${HOME}/.ssh/.auth_sock.$(hostname)}"
 
+# Only do stuff if ssh-agent is not already running
 if ! pgrep -u $USER ssh-agent > /dev/null 2>&1
 then
-
-  # Set new SSH_AUTH_SOCK
-  export SSH_AUTH_SOCK="${HOME}/.ssh/.auth_sock.$(hostname)"
 
   # Run ssh-agent if not already running
   rm -f $SSH_AUTH_SOCK 2> /dev/null
