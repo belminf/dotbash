e797b42f499bdf2dd25f5e84ff2825e5d5d71407
Belmin Fernandez <belmin.fernandez@crowdstrike.com>
2019-01-04 11:49:02 -0500

    some refactor

--- src/k8s.sh
+++ src/k8s.sh
@@ -4,50 +4,53 @@ function kubectl_alias() {
 	command kubectl $@
 }
 
+# Setup kubectl completion
+source <(kubectl completion bash)
+
+# Add completion
+function add_completion() {
+	local complete_cmd=$1
+	local shell_cmd=$2
+
+	if [[ $(type -t compopt) == "builtin" ]]; then
+		complete -o default -F $complete_cmd $shell_cmd
+	else
+		complete -o default -o nospace -F $complete_cmd $shell_cmd
+	fi
+}
+
+# Complete for deployments
+## kubectl doesn't include completion for just deployments so creating one
+function kubectl_complete_deployments() {
+	__kubectl_parse_get "deployment"
+}
+
 # Aliases that need completion
 
 ## Shortcut
 alias k='kubectl'
+add_completion __start_kubectl k
 
 ## Get
 alias kg='kubectl get'
+add_completion __kubectl_get_resource kg
 
 ## Switch context
 alias kc='kubectl_alias config use-context'
+add_completion __kubectl_config_get_contexts kc
 
 ## Get pod image
 alias kimg='kubectl_alias get pods -o "custom-columns=NAME:.metadata.name,STATUS:.status.phase,NODE:.status.hostIP,IMAGE:.spec.containers[*].image"'
+add_completion __kubectl_get_resource_pod kimg
 
 ## Get deployment image
 alias kdimg='kubectl get deployment -o "custom-columns=NAME:.metadata.name,STATUS:.status.phase,NODE:.status.hostIP,IMAGE:.spec.template.spec.containers[*].image"'
+add_completion __kubectl_get_resource_deployment kdimg
 
 ## Kicks off a rolling restart
 ## For annotation, $K8S_DOMAIN in .src/local.sh or defaults to "krestart"
 alias krestart='kubectl_alias patch -p  "{\"spec\":{\"template\":{\"metadata\":{\"annotations\":{\"${K8S_DOMAIN:-krestart}/date\":\"$(date +'%s')\"}}}}}"'
-
-# Auto completion set up
-source <(kubectl completion bash)
-
-## kubectl doesn't include completion for just deployments so creating one
-__kubectl_get_resource_deployment() {
-	__kubectl_parse_get "deployment"
-}
-
-if [[ $(type -t compopt) == "builtin" ]]; then
-	complete -o default -F __start_kubectl k
-	complete -o default -F __kubectl_get_resource kg
-	complete -o default -F __kubectl_config_get_contexts kc
-	complete -o default -F __kubectl_get_resource_pod kimg
-	complete -o default -F __kubectl_get_resource_deployment kdimg
-	complete -o default -F __kubectl_get_resource_deployment krestart
-else
-	complete -o default -o nospace -F __start_kubectl k
-	complete -o default -o nospace -F __kubectl_get_resource kg
-	complete -o default -o nospace -F __kubectl_config_get_contexts kc
-	complete -o default -o nospace -F __kubectl_get_resource_pod kimg
-	complete -o default -o nospace -F __kubectl_get_resource_deployment kdimg
-	complete -o default -o nospace -F __kubectl_get_resource_deployment krestart
-fi
+add_completion __kubectl_get_resource_deployment krestart
 
 # Aliases that do not need completion
 
