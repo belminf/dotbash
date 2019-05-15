51013ad1a62564a40f03afb382cbf1485ab002b1
Belmin Fernandez <belmin.fernandez@crowdstrike.com>
2019-05-14 21:07:02 -0400

    Fixes brew bash completion to be consistent with others

--- src/aaa_complete.sh
+++ src/aaa_complete.sh
@@ -1,9 +1,10 @@
 # Source bash-completion if available
+
 ## Arch
 source /usr/share/bash-completion/bash_completion 2>/dev/null
 
 ## macOS (brew)
-hash brew 2> /dev/null && source $(brew --prefix)/etc/bash_completion 2>/dev/null
+source $(brew --prefix)/etc/bash_completion 2>/dev/null
 
 # Treat hyphens and underscores as equivalent
 bind "set completion-map-case on"
