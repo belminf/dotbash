15451848ff7a085c85f046c8f18a46835bd8dd56
Belmin Fernandez <belmin.fernandez@crowdstrike.com>
2019-12-03 13:13:42 -0500

    Updates tmux-ssh shortcut for new config

--- src/tmux.sh
+++ src/tmux.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 function tmux-ssh() {
-  local tmux_cmd ssh_cmd finish_tmux_setup
-
-  ssh_cmd="ssh"
-  finish_tmux_setup=false
+  local finish_tmux_setup=false
 
   # Exit if I don't have args
   if [[ $# -lt 1 ]]; then
@@ -12,7 +9,7 @@ function tmux-ssh() {
     tmux_cmd="new-window"
     while read -r line; do
       for h in $line; do
-        tmux $tmux_cmd "$ssh_cmd $h"
+        tmux $tmux_cmd "ssh $h"
         tmux_cmd="split-window -h"
         finish_tmux_setup=true
       done
@@ -22,7 +19,7 @@ function tmux-ssh() {
 
     tmux_cmd="new-window"
     for h in "$@"; do
-      tmux $tmux_cmd "$ssh_cmd $h"
+      tmux $tmux_cmd "ssh $h"
       tmux_cmd="split-window -h"
       finish_tmux_setup=true
     done
@@ -30,8 +27,7 @@ function tmux-ssh() {
 
   [[ $finish_tmux_setup ]] || return
 
+  tmux command-prompt -p "window name:" "rename-window '%%'"
   tmux select-layout tiled
-  tmux set-window-option window-status-format "#F:#I:mssh:%1"
-  tmux set-window-option window-status-current-format "#F:#I:mssh:%1"
   tmux set-window-option synchronize-panes
 }
