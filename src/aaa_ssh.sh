a17b2bd137ab618571fdaa23495290f4cccb8c7b
Belmin Fernandez <belmin.fernandez@crowdstrike.com>
2020-07-29 10:39:36 -0400

    Updates ssh-agent set up

--- src/aaa_ssh.sh
+++ src/aaa_ssh.sh
@@ -1,3 +1,6 @@
 export SSH_AUTH_SOCK="${HOME}/.ssh/.ssh_agent"
 
-[ -S "$SSH_AUTH_SOCK" ] || eval "$(ssh-agent -a "$SSH_AUTH_SOCK")"
+if ! pgrep -qf "ssh-agent -a $SSH_AUTH_SOCK" || [ ! -S "$SSH_AUTH_SOCK" ]; then
+  rm -f "$SSH_AUTH_SOCK"
+  eval "$(ssh-agent -a "$SSH_AUTH_SOCK")"
+fi
